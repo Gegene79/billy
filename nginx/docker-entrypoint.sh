8d8cf3658a4ce70632886833e46d94693ade2462
Gegene79 <fabienvignat@yahoo.fr>
2019-12-04 15:13:31 +0100

    versión mas avanzada

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
-envsubst '${NODE_HOST} ${IMAGE_DATA} ${THUMB_DATA}' < /etc/nginx/locations.template > /etc/nginx/locations.conf
-envsubst '${NODE_HOST} ${IMAGE_DATA} ${THUMB_DATA}' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
-cat /etc/nginx/nginx.conf
-cat /etc/nginx/locations.conf
+envsubst '${NODE_HOST} ${IMAGE_DATA} ${THUMB_DATA} ${DH_PARAM}' < /etc/nginx/locations.template > /etc/nginx/locations.conf
+envsubst '${NODE_HOST} ${IMAGE_DATA} ${THUMB_DATA} ${DH_PARAM}' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
+#cat /etc/nginx/nginx.conf
+#cat /etc/nginx/locations.conf
 exec "$@"
