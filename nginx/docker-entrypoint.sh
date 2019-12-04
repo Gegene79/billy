d5d5656ab0306001d95db7380addff9934391d5d
Gegene79 <fabienvignat@yahoo.fr>
2019-12-04 09:15:40 +0100

    modificación config nginx

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 set -eu
+envsubst '${NODE_HOST} ${IMAGE_DATA} ${THUMB_DATA}' < /etc/nginx/locations.template > /etc/nginx/locations.conf
 envsubst '${NODE_HOST} ${IMAGE_DATA} ${THUMB_DATA}' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
 cat /etc/nginx/nginx.conf
 exec "$@"
