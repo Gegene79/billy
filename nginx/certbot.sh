32c34f198a0b63a88cde2e838532c431c190a890
Gegene79 <fabienvignat@yahoo.fr>
2019-12-04 17:06:07 +0100

    nueva versión con todo incluido en nginx container

---
+++ nginx/certbot.sh
@@ -0,0 +1,16 @@
+certbot certonly \
+        --config-dir /letsencrypt \
+		--agree-tos \
+		--domains "$DOMAIN" \
+		--email "$EMAIL" \
+        --no-eff-email \
+		--expand \
+		--noninteractive \
+		--webroot \
+		--webroot-path /certbot \
+	    || true
+
+if [[ -f "/letsencrypt/live/$DOMAIN/privkey.pem" ]]; then
+    cp -f "/letsencrypt}/live/$DOMAIN/privkey.pem" /usr/share/nginx/certificates/privkey.pem
+    cp -f "/letsencrypt}/live/$DOMAIN/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
+fi
--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env sh
+
+
+### Sustitute environment variables in ngnix configuration templates
 set -eu
-envsubst '${NODE_HOST} ${IMAGE_DATA} ${THUMB_DATA} ${DH_PARAM}' < /etc/nginx/locations.template > /etc/nginx/locations.conf
-envsubst '${NODE_HOST} ${IMAGE_DATA} ${THUMB_DATA} ${DH_PARAM}' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
+envsubst '${NODE_HOST} ${DOMAIN}' < /etc/nginx/locations.template > /etc/nginx/locations.conf
+envsubst '${NODE_HOST} ${DOMAIN}' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
 #cat /etc/nginx/nginx.conf
 #cat /etc/nginx/locations.conf
-exec "$@"
+
+### Send certbot Emission/Renewal to background
+$(while :; do /opt/certbot.sh; sleep 24h; done;) &
+
+### Check for changes in the certificate (i.e renewals or first start) and send this process to background
+$(while inotifywait -e close_write /usr/share/nginx/certificates; do nginx -s reload; done) &
+
+### Start nginx with daemon off as our main pid
+nginx -g "daemon off;"
