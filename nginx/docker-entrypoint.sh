621ccdd9da988c53bc3523607a119bac63542051
Gegene79 <fabienvignat@yahoo.fr>
2019-12-04 10:01:59 +0100

    corrección error

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -3,4 +3,5 @@ set -eu
 envsubst '${NODE_HOST} ${IMAGE_DATA} ${THUMB_DATA}' < /etc/nginx/locations.template > /etc/nginx/locations.conf
 envsubst '${NODE_HOST} ${IMAGE_DATA} ${THUMB_DATA}' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
 cat /etc/nginx/nginx.conf
+cat /etc/nginx/locations.conf
 exec "$@"
